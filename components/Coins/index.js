291ca00a69479041fa52193955779d3b29745fcf
maxcore25 <mkorobov2002@mail.ru>
2022-04-20 09:26:53 +0300

    Coins JSX Copied

--- components/Coins/index.js
+++ components/Coins/index.js
@@ -1,11 +1,48 @@
 import styles from './Coins.module.scss';
+import Link from 'next/link';
 
-const Coins = ({ name, price }) => {
+const Coins = ({
+  name,
+  price,
+  symbol,
+  marketcap,
+  volume,
+  image,
+  priceChange,
+  id,
+}) => {
   return (
-    <div>
-      <h1>{name}</h1>
-      <p>{price}</p>
-    </div>
+    <Link href='/coin/[id]' as={`/coin/${id}`}>
+      <a>
+        <div className={styles.coin_container}>
+          <div className={styles.coin_row}>
+            <div className={styles.coin}>
+              <img src={image} alt={name} className={styles.coin_img} />
+              <h1 className={styles.coin_h1}>{name}</h1>
+              <p className={styles.coin_symbol}>{symbol}</p>
+            </div>
+            <div className={styles.coin_data}>
+              <p className={styles.coin_price}>${price}</p>
+              <p className={styles.coin_volume}>${volume.toLocaleString()}</p>
+
+              {priceChange < 0 ? (
+                <p className={(styles.coin_percent, styles.red)}>
+                  {priceChange.toFixed(2)}%
+                </p>
+              ) : (
+                <p className={(styles.coin_percent, styles.green)}>
+                  {priceChange.toFixed(2)}%
+                </p>
+              )}
+
+              <p className={styles.coin_marketcap}>
+                Mkt Cap: ${marketcap.toLocaleString()}
+              </p>
+            </div>
+          </div>
+        </div>
+      </a>
+    </Link>
   );
 };
 
