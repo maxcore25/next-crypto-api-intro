13b7a46938bceaf40b785b2e7d2e01b56771dc1b
maxcore25 <mkorobov2002@mail.ru>
2022-04-20 09:11:49 +0300

    SSR Function Added

--- pages/index.js
+++ pages/index.js
@@ -17,3 +17,14 @@ export default function Home() {
     </div>
   );
 }
+
+export async function getServerSideProps(context) {
+  const res = await fetch(
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+  );
+  const filteredCoins = await res.json();
+
+  return {
+    props: { filteredCoins },
+  };
+}
